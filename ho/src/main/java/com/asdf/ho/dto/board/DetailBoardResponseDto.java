5bb4e6ebca9a22e86fc68f54cfe4bb563d37da96
mylhb9 <mylhb9@naver.com>
2021-12-09 09:24:56 +0900

    Front 요구사항 유효성 검사 및 boardId 값 적용

--- ho/src/main/java/com/asdf/ho/dto/board/DetailBoardResponseDto.java
+++ ho/src/main/java/com/asdf/ho/dto/board/DetailBoardResponseDto.java
@@ -18,9 +18,10 @@ public class DetailBoardResponseDto {
     private String content;
     private String img;
     private LocalDateTime modifiedAt;
-    private List<CommentDto> comments;
-    //조회수
     private int view;
+    private List<CommentDto> comments;
+
+
 
     @Builder
     public DetailBoardResponseDto(String username, String title, String content, String img, LocalDateTime modifiedAt, List<CommentDto> comments, int view) {
